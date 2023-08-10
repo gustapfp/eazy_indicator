d8c5eea3813873f7b35965a178cdb85abb2492dc
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2023-08-10 14:12:37 +0200

    setup with config.py and db connection

---
+++ backend/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from core.config import settings
+from beanie import init_beanie 
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+@app.on_event('startup')
+async def app_init():
+    client_db = AsyncIOMotorClient(settings.MONGO_CONNECTION_STRING).eazy_indicator
+    await init_beanie(
+        database= client_db,
+        document_models=[
+            
+        ]
+    )
